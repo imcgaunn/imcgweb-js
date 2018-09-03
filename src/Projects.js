6a498b87746a3eb6201ae9bd347c5fbb6f3abc0e
Ian McGaunn <ianmcgaunn@pressmail.ch>
2018-09-03 10:54:37 -0400

    add a working example of the fetch api. needed to use a fetch polyfill for jest tests.

--- src/Projects.js
+++ src/Projects.js
@@ -1,45 +1,23 @@
 import React from "react";
 import https from "https";
+import fetch from "isomorphic-fetch";
 
 const githubHostName = "api.github.com";
 const myUserName = "imcgaunn";
 
 const fetchProjects = username => {
-  console.log("fetching projects");
-  const requestOptions = {
-    hostname: githubHostName,
-    path: `/users/${username}/repos`,
-    method: "GET",
-    port: 443,
-    headers: {
-      'User-Agent': 'coolagent'
-    },
-  };
-  https.request(requestOptions, res => {
-    const { statusCode } = res;
-    let rawData = "";
-
-    res.setEncoding("utf8");
-    res.on("data", chunk => {
-      rawData += chunk;
-    });
-    res.on("end", () => {
-      try {
-        console.log(rawData);
-        const parsedData = JSON.parse(rawData);
-        console.log(parsedData);
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
-    res.on("error", e => {
-      console.error(`Got error: ${e.message}`);
-    });
+  const url = `https://${githubHostName}/users/${username}/repos`;
+  return fetch(url).then(resp => {
+    return resp.json();
   });
 };
 
-fetchProjects("imcgaunn");
+const ProjectsPage = props => {
+  return (
+    <div>
+      <p> this is a great paragraph boiiiii </p>
+    </div>
+  );
+};
 
-console.log("nice");
-
-export { fetchProjects };
+export { fetchProjects, ProjectsPage };
--- src/Projects.test.js
+++ src/Projects.test.js
@@ -1,5 +1,19 @@
-import { fetchProjects } from './Projects';
+import { fetchProjects } from "./Projects";
+import { fromJS, Map } from "immutable";
 
-it('can return a json list of projects', () => {
-  fetchProjects('imcgaunn');
+it("can return a json list of projects", () => {
+  fetchProjects("imcgaunn")
+    .then(data => {
+      const immutableData = fromJS(data);
+      immutableData.map(proj => {
+        const interestingData = Map({
+          url: proj.get('html_url'),
+          pushed_at: proj.get('pushed_at')
+        });
+        console.log(interestingData);
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
