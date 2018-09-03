fd64f1c9f62b9a756560b3356e05dc668977cc0e
Ian McGaunn <ianmcgaunn@pressmail.ch>
2018-09-03 13:57:28 -0400

    get rid of remaining class-style components in favor of 'functional' components.

--- src/Common.js
+++ src/Common.js
@@ -1,30 +1,20 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router,
-  Route, 
-  Link } from 'react-router-dom';
-import { HomePage } from './App';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HomePage } from "./App";
 
-class CommonHeader extends Component {
-  render() {
-    return (
-      <header class="mainHeader">
-        <h2>{this.props.title}</h2>
-      </header>
-    );
-  }
-}
+const CommonHeader = props => {
+  return (
+    <header class="mainHeader">
+      <h2>{this.props.title}</h2>
+    </header>
+  );
+};
 
-class CommonFooter extends Component {
-  render() {
-    const footerStyles = {
-      textAlign: 'center'
-    };
-    return (
-      <footer style={footerStyles}>
-        ;0
-      </footer>
-    );
-  }
-}
+const CommonFooter = props => {
+  const footerStyles = {
+    textAlign: "center"
+  };
+  return <footer style={footerStyles}>;0</footer>;
+};
 
 export { CommonHeader, CommonFooter };
